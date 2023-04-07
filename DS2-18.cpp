a746e93d2ee9b745add81e894c70969ebd660728
aliamnieh <117279015+aliamnieh@users.noreply.github.com>
2023-04-07 10:44:28 -0700

    Add files via upload

---
+++ DS2-18.cpp
@@ -0,0 +1,36 @@
+﻿// DS2-18.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+//در صفحه ی شطرنج مقابل میخواهیم از نقطه ی الف به نقطه ب برویم. یک رابطه بازگشتی برای ان بنویسید
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+//a function for counting the number of ways from A to B
+long int f(int m, int n) {
+    if (m == 0 || n == 0)return 1;
+    return (f(m - 1, n) + f(m, n - 1));
+}
+
+int main()
+{
+    int row, column;
+    cout << "row = ";
+    cin >> row;
+    cout << "column = ";
+    cin >> column;
+    cout << f(column, row);
+    _getch();
+    return 0;
+}
+
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
---
+++ DS2-19.cpp
@@ -0,0 +1,31 @@
+﻿// DS2-19.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+//یک رابطه ی بازگشتی برای تعداد ضرب ها در تابع فاکتوریل بنویسید
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+long int fact(int x,int& count) {
+    if (x == 0)return 1;
+    count++;//a variable to count the number of * loading
+    return x * fact(x - 1, count);
+}
+int main()
+{
+    int count = 0;
+    cout << fact(10, count) << endl;
+    cout << count;
+    _getch();
+    return 0;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
